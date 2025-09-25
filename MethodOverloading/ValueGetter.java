9365f5ce0cf838837ad370030d082465cc845c75
mcorridor <corridor.marlo@gmail.com>
2025-09-26 02:26:14 +0800

    Set functions to private and implement constants (final keyword)

--- MethodOverloading/ValueGetter.java
+++ MethodOverloading/ValueGetter.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
 
 public class ValueGetter {
   // 1 for Integer, 2 for String
+  private final int TYPE_INT = 1;
+  private final int TYPE_STRING = 2;
   private final int data_type;
   private final int record_count;
   private final Scanner scanner;
@@ -15,12 +17,12 @@ public class ValueGetter {
 
   // utility method to print prompt and get values
   private void askForValues() {
-    System.out.println("You selected data type: " + (data_type == 1 ? "Integer" : "String"));
+    System.out.println("You selected data type: " + (data_type == TYPE_INT ? "Integer" : "String"));
     System.out.println("You will enter " + record_count + " records.");
   }
 
   // Method to get integer values
-  public int[] getIntValues() {
+  private int[] getIntValues() {
     int[] values = new int[this.record_count];
     this.askForValues();
     for (int i = 0; i < this.record_count; i++) {
@@ -30,7 +32,7 @@ public class ValueGetter {
   }
 
   // Method to get string values
-  public String[] getStringValues() {
+  private String[] getStringValues() {
     String[] values = new String[this.record_count];
     this.askForValues();
     for (int i = 0; i < this.record_count; i++) {
@@ -41,9 +43,9 @@ public class ValueGetter {
 
   // Method to get values based on data_type, returns Object for generic handling
   public Object getValues() {
-    if (data_type == 1) {
+    if (data_type == TYPE_INT) {
       return getIntValues();
-    } else if (data_type == 2) {
+    } else if (data_type == TYPE_STRING) {
       return getStringValues();
     }
     return null;
